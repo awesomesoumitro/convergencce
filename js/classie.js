7196ccc7d3b518c6b9f7db2d3eb6c638826c332e
unknown <raw08smackdown@gmail.com>
2015-09-30 17:29:27 +0530

    mostly done

--- js/classie.js
+++ js/classie.js
@@ -1,15 +1,5 @@
-/*!
- * classie - class helper functions
- * from bonzo https://github.com/ded/bonzo
- * 
- * classie.has( elem, 'my-class' ) -> true/false
- * classie.add( elem, 'my-new-class' )
- * classie.remove( elem, 'my-unwanted-class' )
- * classie.toggle( elem, 'my-class' )
- */
 
 /*jshint browser: true, strict: true, undef: true */
-/*global define: false */
 
 ( function( window ) {
 
@@ -55,7 +45,7 @@ function toggleClass( elem, c ) {
   fn( elem, c );
 }
 
-var classie = {
+window.classie = {
   // full names
   hasClass: hasClass,
   addClass: addClass,
@@ -67,14 +57,5 @@ var classie = {
   remove: removeClass,
   toggle: toggleClass
 };
-
-// transport
-if ( typeof define === 'function' && define.amd ) {
-  // AMD
-  define( classie );
-} else {
-  // browser global
-  window.classie = classie;
-}
 
 })( window );
